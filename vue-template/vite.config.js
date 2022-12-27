09735194dabf574c450871c17f3b9682de43a3cb
moonRider <solar.pom@gmail.com>
2022-12-27 15:59:47 +0800

    chore: update template's vite config
    - remove base config, it should be set by `vite build --base=xxx`

--- vue-template/vite.config.js
+++ vue-template/vite.config.js
@@ -1,14 +1,11 @@
 import { URL, fileURLToPath } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig(() => {
   return {
-    base: env.BASE_URL,
     plugins: [vue()],
     resolve: {
       alias: {
