db41aeb9adc61fca2c70536000b051a7b5f18871
周小琪 <37129846+zxqzy@users.noreply.github.com>
2023-02-02 11:28:05 +0800

    feat: support tailwindss and sass

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,31 +1,12 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
+  // @if !tailwindcss
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
+  // @endif
+  // @if tailwindcss
+  <h1 class="text-gray-900">{{ msg }}</h1>
+  // @endif
 </template>
+
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
--- vue/src/views__if_router/home/index.vue
+++ vue/src/views__if_router/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vue + TypeScript + Vite" />
 </template>
 
 <script setup lang="ts">
