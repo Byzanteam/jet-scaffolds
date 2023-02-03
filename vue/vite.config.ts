c3ba1688070518b8f864fee6fda828069296c154
周小琪 <37129846+zxqzy@users.noreply.github.com>
2023-02-03 14:44:50 +0800

    feat: support auto import

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,6 +1,11 @@
 import { URL, fileURLToPath } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
+// @if auto-import
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+// @endif
 import { defineConfig, loadEnv } from 'vite'
 // @if router
 import AutoRoutesPlugin from 'vite-plugin-pages'
@@ -14,6 +19,27 @@ export default defineConfig(({ mode }) => {
     base: env.BASE_PATH,
     plugins: [
       vue(),
+      // @if auto-import
+      AutoImport({
+        // @if !router
+        imports: ['vue'],
+        // @endif
+        // @if router
+        imports: ['vue', 'vue-router'],
+        // @endif
+        resolvers: [ElementPlusResolver()],
+        dts: '.typings/auto-import.d.ts',
+        eslintrc: {
+          enabled: true,
+          filepath: '.typings/.eslintrc-auto-import.json',
+        },
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+        dts: '.typings/components.d.ts',
+        dirs: ['src/components'],
+      }),
+      // @endif
       // @if router
       AutoRoutesPlugin({
         routeStyle: 'nuxt',
