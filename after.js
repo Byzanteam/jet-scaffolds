61a720d2307b1a8adab82fead6ead96149f6bf50
moonRider <solar.pom@gmail.com>
2023-02-08 15:26:20 +0800

    feat: add after.js task and remove prepare script

---
+++ after.js
@@ -0,0 +1,34 @@
+// 模块定义规范必须和 package.json 的 type field 相匹配，否则不会执行
+export default async function({ run, properties, prompts, ansiColors }) {
+  const result = await prompts.select({
+    message: 'Do you want to install dependencies now?',
+    hint: 'pnpm install',
+    choices: [
+      { title: 'Yes', value: 'pnpm' },
+      { title: 'No' }
+    ]
+  })
+
+  // setup git and husky
+  await run('git', ['init', '-b', 'main'])
+  await run('pnpm', ['dlx', 'husky', 'install'])
+
+  if (result) {
+    await run('pnpm', 'install')
+  }
+
+  console.log(ansiColors.cyan(`[makes] Project ${properties.name} has been initialized.`))
+  console.log(ansiColors.cyan('follow next steps to get started.'))
+
+  const nextSteps = [
+    `cd ${properties.name}`,
+  ]
+
+  if (!result) {
+    nextSteps.push('pnpm install')
+  }
+
+  for (const step of nextSteps) {
+    console.log(` › ${step}`)
+  }
+}
--- vue/README.md
+++ vue/README.md
@@ -14,7 +14,6 @@ pnpm install
 
 # 本地如需覆盖 `.env` 文件，需新建 `.env.local` 文件
 # 环境变量设置参考 https://vitejs.dev/guide/env-and-mode.html#env-files
-
 pnpm dev
 
 # 打包生产环境代码
