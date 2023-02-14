cd4cf2bf7664ed64a11f5fb9046eab2b26752dad
moonRider <solar.pom@gmail.com>
2023-02-14 13:07:31 +0800

    fix: vue-router with BASE_PATH error

--- vue/src/router__if_router/index.ts
+++ vue/src/router__if_router/index.ts
@@ -3,7 +3,7 @@ import routes from '~pages'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_PATH),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
