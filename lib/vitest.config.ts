0ba3f1347468e7423f653a2482fbbdca76b80b8e
shaddollxz <56341682+shaddollxz@users.noreply.github.com>
2023-08-03 15:11:18 +0800

    Feat/shaddollxz/lib template (#74)
    添加为浏览器端提供的库模式模版，内置支持 changeset，vitest，可选 vue 框架模版

---
+++ lib/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    plugins: [],
+    test: {
+        testTimeout: 10000,
+        environment: "jsdom",
+        include: ["**/*.(test|spec).[jt]s?(x)"],
+        passWithNoTests: true,
+    },
+});
